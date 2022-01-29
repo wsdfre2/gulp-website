e070db49e2ef584604a7c94d89167347a1d61a30
wsdfre2 <youngc0905@gmail.com>
2022-01-28 22:06:32 -0800

    updated packages

---
+++ .cache/api-runner-browser-plugins 5.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Absurd","short_name":"absurd","start_url":"/","background_color":"#8bd8ed","theme_color":"#8bd8ed","display":"minimal-ui","icon":"static/favicon.svg","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"0ed3b0bd00cc7c1a1f9fda808e6a813e"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
